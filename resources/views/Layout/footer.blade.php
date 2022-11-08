0a578fc89025d36de7c31f42498df077224507d9
chngesjey <nandoaja901@gmail.com>
2022-11-08 15:34:52 +0700

    FrontEnd Done

---
+++ resources/views/Layout/footer.blade.php
@@ -0,0 +1,11 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.25s">
+                    <p>© Copyright 2021 Space Dynamic Co. All Rights Reserved.
+
+                        <br>Design: <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
